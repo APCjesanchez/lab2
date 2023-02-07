eafed23e4a6440831d4efa0022b177bba295e270
Johanne Marlowe E. Sanchez <119918311+APCjesanchez@users.noreply.github.com>
2023-02-07 15:27:54 +0800

    Guests PHP for New Database

--- week9/guests.php
+++ week9/guests.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname FROM jesanchez_myguests";
+$sql = "SELECT id, name, email, website, comment, gender, reg_date FROM jesanchez_myguest";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . "<br>";
+        echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " - Email: " . $row["email"] . "<br>";
     }
 } else {
     echo "0 results";
